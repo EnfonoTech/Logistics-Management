382b9e07f7b50a522af1e157f56c58cb55de75a7
sivajyothis7 <sivajyothis9446@gmail.com>
2024-09-06 12:49:49 +0530

    new

--- logistics_management/logistics_management/doctype/job_details/job_details.py
+++ logistics_management/logistics_management/doctype/job_details/job_details.py
@@ -2,19 +2,20 @@ import frappe
 from frappe.model.document import Document
 
 class JobDetails(Document):
-    def before_save(self):
-        if self.company == 'HARBOUR SHIPPING AND MARINE SERVICES WLL-NVOCC DIVISSION':
-            job_numbers = frappe.get_all('Job Details', filters={'name': ['like', 'HSMN%']}, fields=['name'], order_by='cast(substr(name, 5) as unsigned) ASC')
-            job_numbers = [int(job['name'][4:]) for job in job_numbers]
+    pass
+    # def before_save(self):
+    #     if self.company == 'HARBOUR SHIPPING AND MARINE SERVICES WLL-NVOCC DIVISSION':
+    #         job_numbers = frappe.get_all('Job Details', filters={'name': ['like', 'HSMN%']}, fields=['name'], order_by='cast(substr(name, 5) as unsigned) ASC')
+    #         job_numbers = [int(job['name'][4:]) for job in job_numbers]
             
-            next_job_number = 1001  
-            for number in job_numbers:
-                if number != next_job_number:
-                    break
-                next_job_number += 1
+    #         next_job_number = 1001  
+    #         for number in job_numbers:
+    #             if number != next_job_number:
+    #                 break
+    #             next_job_number += 1
 
-            next_job_number_formatted = 'HSMN{:04}'.format(next_job_number)
-            self.name = next_job_number_formatted
+    #         next_job_number_formatted = 'HSMN{:04}'.format(next_job_number)
+    #         self.name = next_job_number_formatted
 
 
     # def get_options(self):
@@ -31,30 +32,30 @@ class JobDetails(Document):
 
     #     return next_value_formatted
 
-@frappe.whitelist()
-def generate_project(docname):
-    if not docname:
-        frappe.msgprint("Document name is required to generate the waybill.")
-        return
+# @frappe.whitelist()
+# def generate_project(docname):
+#     if not docname:
+#         frappe.msgprint("Document name is required to generate the waybill.")
+#         return
 
-    doc = frappe.get_doc("Job Details", docname)
+#     doc = frappe.get_doc("Job Details", docname)
 
-    if doc.project:
-        frappe.throw(f"Project already exists for this Job: {doc.project}")
-    else:
-        # Check if the naming series needs to be set for SecOnd Company
-        if doc.company == 'SecOnd Company':
-            doc.before_save()  # Ensure naming series logic is applied
+#     if doc.project:
+#         frappe.throw(f"Project already exists for this Job: {doc.project}")
+#     else:
+#         # Check if the naming series needs to be set for SecOnd Company
+#         if doc.company == 'SecOnd Company':
+#             doc.before_save()  # Ensure naming series logic is applied
 
-        data = frappe.get_doc({
-            "doctype": "Project",
-            "project_name": doc.name,
-            "custom_job_details": doc.name,
-        })
-        data.insert(ignore_permissions=True)
-        frappe.msgprint(f"Project generated successfully: {data.name}")
+#         data = frappe.get_doc({
+#             "doctype": "Project",
+#             "project_name": doc.name,
+#             "custom_job_details": doc.name,
+#         })
+#         data.insert(ignore_permissions=True)
+#         frappe.msgprint(f"Project generated successfully: {data.name}")
 
-        doc.project = data.name
-        doc.save(ignore_permissions=True)
+#         doc.project = data.name
+#         doc.save(ignore_permissions=True)
 
-        return doc.project
+#         return doc.project
